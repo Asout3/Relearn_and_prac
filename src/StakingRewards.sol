3588399762b4aba3b57818a45139185f4f2972cd
Asout3 <mikiyasyimerm@gmail.com>
2026-06-15 04:04:41 +0000

    still working on it

--- src/StakingRewards.sol
+++ src/StakingRewards.sol
@@ -191,7 +191,93 @@ LESSON: NEVER RUSH TO WRITE A CODE FIRST UNDERSTAND WHAT YOU ARE TRYING TO DO, U
         THEN YOU CAN WRITE THE CODE AND WRITING THE CODE BECOME MUCH EASIER AND YOU CAN REVIEW YOUR CODE REFACTOR 
         SINCE YOU KNOW WHAT YOU ARE TRULY TRYING TO DO IT IS GOING TO BE EASY. AT LEAST LIKE KNOW WHAT YOU ARE TRYING TO DO.
 
+        - And it will save you a lot of hours fight bugs.
+
+        
  */
+
+ /**
+ okay lets start i am going to break this shit down and like try to understand this thing
+ okay i will try to understand from the core the thing is we the idea is there is this stalk and reward thing
+ which the user profit from stalking here is the basic math
+
+ I WILL CONTINUE THAT WHEN I GET BACK ON THE NEXT SESSION.
+
+so what i am going to do is go and read and understand as much as possible i don't want stupid ai explantion. 
+  okay updates i couldn't find shit there is no proper information on the interent that could be understandable they all short and not proper and doesn't even care to explain the maths
+  so i going to ask ai to explain it to me for further understanding.
+  
+
+- okay this is what i know so far: 
+so synthix reward staking is a stake and get reward game you stake and you get rewared based on the stake. i don't really know where the stake comes from like i don't know the system invest it or smt else but what i know is like the get proffit the owner distribute the money.
+and we accept an erc20 and we give an erc20 token back as a reward token it is also erc20
+
+so let me dive into the arctecture so this kind of contracts doesn't really have an erc or something like that they just copy one another and use it so i will try to make this one as best as possible also from the instruction and what i read.
+
+so let me explain this thing the main thing important part is like the 'rewardPerTokenStored' the reason we use that is instead of updating each user state we can like use this variable to track how much reward per token get like for each token how much reward it gets
+so like if we use loop to update each user state the contract will be unuseable for gas reason and also eventuall stop working for the case of dos cuz it will use more gas than ethereum want it so it will be dos and if that didn't happen if we allow a lot of gas it will be expensive and no one will use it so this is genius.
+let me explain to you how the 'rewardPerTokenStored' works so every time the state change like when we do some action that changes the state of the stake we put our rewardPerToken and here is how it works
+
+rewardPerToken += (rewardRate * timePassed * 1e18) / totalStaked 
+
+so it means add to rewardPerToken :  
+rewardRate means how much toekn we give per day like the reward rate per day or per minute or sec.
+timePassed means how much time passed like how many reward where given like it is kinda last updated time.
+1e18 is like solidity doesn't have decimal so like we use that to neutalize it 
+totalStaked is like total amount of staked token
+
+so as i told you the use of this reward per token is like it gives us how much reward per a token gets if you have one token how much reward would that get.
+
+so like for this image to form up let me add one other core feature and like it is going to give the full picture and you understand how it works
+so remeber the core thing is that stake and reward distribution based on how much you staked and to give you how much you earned i showed you the first step which is rewardPerToken.
+the second most important part is the userRewardPerTokenPaid this is also important thing also why we use it to calculate how much let me answer that.
+we use userRewardPerTokenPaid to track the rewardPerToken of that user what i mean is like so every time rewardPerToken is updated so like and since it is updated every time we don't want to give new
+comers the reward they don't really owe so we use rewardPerToken as a timestamp typeshit so like we put when ever they join like when ever they join we add to their name the current reward per token let me show you example.
+example: 
+current staked amount = 200
+current reward per toekn = 3
+
+so alice got in to day
+she provide 100 amount to stake so the current staked amount is 300
+the userRewardPerTokenStored[alice address] = 3
+
+so lets say 2 or 3 days passes more reward distributed those days.
+lets say the staked amount is like = 500 // other people got in also
+current reward per token = 6
+
+so alice wants to cliam this is where the magic happens when she hit cliam this is what happens
+it calculates 
+amount of reward to claim = stakedBalance[alice] * (rewardPerTokenStored - userRewardPerTokenPaid[alice]) / 1e18 // for precsison.
+ aortw or rewards[user] = (6 - 3) * 100 // lets try to forget the 1e18 for now okay.
+ = 3 * 100
+ = 300 reward tokens   so all this goes to the mapping rewards[] i will explain how the reward works soon.
+
+and also after we finish this calcuation we need to updates state whic his like userRewardPerTokenPaid[alice] = current rewardPerTokenStored.
+
+ NOTE: so i may have said things updates constantly but no smart contract can't intialize them self by their own so someone need to intereact with them so when ever someone intereact with them so we updates state whenever someone intereact with them.
+
+so now let me talk about the reward system.
+
+okay i am back lets continue so like i will explain about the reward system which is like the last part and the most important thing so ya lets start.
+so the reward system works like in the contract the owner deposite the amount to distribute on the contract so like we can check it using like balanceOf(address(this)) so 
+after the owner deposite they call notifyReward(uint256 amount) so like the amount of reward token he wants to distribute this week.
+so like after it gets notified we gonna calcuate the rewardRate so like this reward rate counts like per second so we gonna add a lot of second in it so it will be like day it is like the system it only count seconds so nothing we can do about it okay so it will be like 86400 seconds.
+so the formual is like 
+    rewardRate = rewardAmount / 7 days  so like it will be the notified amount / 86400 * 7 seconds
+    so like this rewardRate formula is like in the notify reward function so like we use that and it is good
+    and what i want to stress is like the smart contract update it self so like when ever someone interact it does all the updates like like update the whole thing then it does it works.
+    so after we calcualte the like the notify reward so we use rewardRate every where we want like in the formulas
+    like we use here:
+    rewardPerTokenStored += (rewardRate * timePassed * 1e18) / totalStaked  //you can see it and every where
+    so like we update every time some one interact like update the time like lastUpdateTime.
+    so also like let me talk a little about the reward[user] mapping so we put the amount of reward that a person can claim and we clean it up after they finish so that is how we use it.
+
+    one important thing left why do we use like why do we use lastUpdateTime so let me clarify that so we use it cuz as i said and you know smart contracts can't intialize them self unless someone calls them the don't wake up
+    so the reward is calcualted could be wrong if it didn't updated right like if someone didn't intereact for like 2 or 3 days we don't know when was the time it was updated so like we don't know how much reward to like diversify 
+    so user lastUpdatedTime so like we could track when was the last time updated then we gonna be able to calculate how much it need to give and do based on our formula so it is importnat
+
+    so that is what i belive i need to explain next i will start the coding part and i will write as much comment and as best quility code as possible thank you.
+  */
 
 contract StakingRewards {
     // two tokens
